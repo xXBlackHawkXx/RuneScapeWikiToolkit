38db6612b7076a38e0fd94c5432d2d4de3faa69b
BlackHawk <xxblack0hawkxx@gmail.com>
2026-03-13 23:43:05 +0000

    Add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: '/RuneScapeWikiToolkit/',
   plugins: [react()],
   resolve: {
     alias: {
